f6dd2d3556f97e0b16c51f29778f9577424d7e37
Kiryl Yasinsky <kirillinski@icloud.com>
2023-10-15 09:10:37 +0300

    добавил слежение камеры за игроком и дверь

---
+++ Assets/ChangeLevel.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChangeLevel : MonoBehaviour
+{
+    public int sceneBuildIndex;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other) {
+        print ("Trigger Entered");
+        if(other.tag == "Player"){
+            print("Switching Scene to " + sceneBuildIndex);
+            SceneManager.LoadScene(sceneBuildIndex,LoadSceneMode.Single);
+        }
+    }
+}

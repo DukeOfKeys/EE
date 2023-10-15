3f46aae2829ff3217af8041e39d1dc9df1131c89
Klivan49 <kliiivan49@gmail.com>
2023-10-15 09:03:28 +0300

    фикс ускоренного движения по диагонали

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -1,26 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] public float moveSpeed = 5f;
+    private float tempSpeed;
     private Rigidbody2D rb;
     private Vector2 movementDirection;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        tempSpeed = moveSpeed;
     }
 
     void Update()
     {
-        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movementDirection.x = Input.GetAxisRaw("Horizontal");
+        movementDirection.y = Input.GetAxisRaw("Vertical");
+        if (movementDirection.x != 0 && movementDirection.y != 0) 
+        {
+            tempSpeed = moveSpeed;
+            tempSpeed = Convert.ToInt32(Convert.ToInt64(tempSpeed) / Math.Sqrt(2));
+        }
+        else
+        {
+            tempSpeed = moveSpeed;
+        }
     }
 
     void FixedUpdate()
     {
-        rb.velocity = movementDirection * moveSpeed;
+        rb.MovePosition(rb.position + movementDirection * tempSpeed * Time.fixedDeltaTime);
     }
+
 }

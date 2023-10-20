3d52d9cdd9b96e18a563452393401a7857a93e80
Klivan49 <kliiivan49@gmail.com>
2023-10-20 22:13:25 +0300

    --мерцание

--- Assets/scripts/CameraMovement.cs
+++ Assets/scripts/CameraMovement.cs
@@ -8,8 +8,7 @@ public class CameraMovement : MonoBehaviour
     public float smoothing;
     public Vector2 maxPosition;
     public Vector2 minPosition;
-    // Update is called once per frame
-    void LateUpdate()
+    void FixedUpdate()
     {
         Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
         targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);

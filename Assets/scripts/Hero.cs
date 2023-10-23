ead01e9d4b35c7fbdd82a13f566d34b8a907f17e
Klivan49 <kliiivan49@gmail.com>
2023-10-23 20:39:15 +0300

    убрал мусор

--- Assets/scripts/Hero.cs
+++ Assets/scripts/Hero.cs
@@ -7,14 +7,6 @@ using System;
 public class Hero : MonoBehaviour
 {
     [SerializeField] public float moveSpeed = 5f;
-    [SerializeField] public byte lives = 5;
-    [SerializeField] public static byte damage = 1;
-    [SerializeField] public float stamina = 100f;
-    [SerializeField] public float timeBtwAttack = 0.7f;
-    [SerializeField] public float attackRange = 10f;
-    public LayerMask enemy;
-    public Transform attackPos;
-    public float reloadTime = 0.1f;
     public float tempSpeed;
     public Rigidbody2D rb;
     public Vector2 movementDirection;
@@ -30,27 +22,11 @@ public class Hero : MonoBehaviour
     void Update()
     {
         MoveContraller();
-        if (stamina >= 20 && timeBtwAttack == 0.7f && Input.GetButtonDown("Fire1"))
-        {
-            Attack();
-        }
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movementDirection * tempSpeed * Time.fixedDeltaTime);
-        StartCoroutine("Reload");
-    }
-
-    void Attack()
-    {
-        stamina -= 20;
-        reloadTime = 0f;
-        Collider2D[] enemies = Physics2D.OverlapCircleAll(attackPos.position, attackRange, enemy);
-        for (int i = 0; i < enemies.Length; i++)
-        {
-            enemies[i].GetComponent<Enemy>().TakeDamage();
-        }
     }
 
     void MoveContraller()
@@ -66,32 +42,5 @@ public class Hero : MonoBehaviour
         {
             tempSpeed = moveSpeed;
         }
-    }
-
-    public void TakeDamage()
-    {
-        lives -= Enemy.damage;
-        if (lives <= 0)
-        {
-            Destroy(gameObject);
-            Debug.Log("борец с чурками отправился на покой");
-        }
-    }
-    IEnumerator Reload()
-    {
-        while (stamina != 100 || reloadTime != timeBtwAttack)
-        {
-            stamina+=0.1f;
-            reloadTime += .1f;
-            if (reloadTime > timeBtwAttack) reloadTime = timeBtwAttack;
-            if (stamina > 100) stamina = 100;
-            yield return new WaitForSeconds(20);
-        }
-    }
-    
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(attackPos.position, attackRange);
     }
 }

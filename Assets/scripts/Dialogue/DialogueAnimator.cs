171afd100315fde3f7ffcce8ec3b99ade526ddf1
Kiryl Yasinsky <kirillinski@icloud.com>
2023-10-31 03:11:58 +0300

    диалоги работают

--- Assets/scripts/Dialogue/DialogueAnimator.cs
+++ Assets/scripts/Dialogue/DialogueAnimator.cs
@@ -1,31 +1,95 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class DialogueAnimator : MonoBehaviour
 {
     private bool check;
+    public GameObject StartDialogueText;
+    [SerializeField] public GameObject DialogueBox;
+    public string[] lines;
+    public float speedText;
+    public Text dialogueText;
+    public int index;
     private void StartDialogue()
     {
         DialogueBox.SetActive(true);
-        FindObjectOfType<DialogueSystem>().endDialogue();
-        FindObjectOfType<DialogueSystem>().startDialogue();
+        endDialogue();
+        startDialogue();
     }
+
+
+
+    /// <summary>
+    public void startDialogue()
+    {
+        dialogueText.text = string.Empty;
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            dialogueText.text += c;
+            yield return new WaitForSeconds(speedText);
+        }
+    }
+
+    private void nextLines()
+    {
+        if (index <= lines.Length - 1)
+        {
+            index++;
+            dialogueText.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            DialogueBox.SetActive(false);
+        }
+    }
+
+    public void skipTextClick()
+    {
+        if ((lines.Length - 1) <= index) {
+ 
+                DialogueBox.SetActive(false);
+            StartDialogueText.SetActive(false);
+        }
+        if (dialogueText.text == lines[index])
+        {
+            nextLines();
+        }
+        else
+        {
+            StopAllCoroutines();
+            dialogueText.text = lines[index];
+        }
+    }
+    public void endDialogue()
+    {
+        dialogueText.text = string.Empty;
+    }
+    /// </summary>
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) & check == true )
+        if (Input.GetKeyDown(KeyCode.Space) & check == true)
         {
             StartDialogueText.SetActive(false);
             StartDialogue();
             check = false;
+        } else if (Input.GetKeyDown(KeyCode.Space) == true){
+            print("Максим Ебаная чурка!!!!");
+            skipTextClick();
         }
     }
     private void Start()
     {
         StartDialogueText.SetActive(false);
     }
-    public GameObject StartDialogueText;
-    public GameObject DialogueBox;
+
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.name == "hero")
